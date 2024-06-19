7483bae8b81993535b60cd21e9bb8b393871a7c2
advansoftware <brunoantunes94@hotmail.com>
2024-06-19 00:47:55 -0300

    melhorando mensagens e adicionando função de atualizar bibliotecas

--- api/controllers/WebhookController.js
+++ api/controllers/WebhookController.js
@@ -1,9 +1,8 @@
+const axios = require("axios");
+
 // api/controllers/WebhookController.js
-const { Telegraf } = require("telegraf");
-const axios = require('axios');
 module.exports = {
   create: async function (req, res) {
-    const bot = new Telegraf("6932592891:AAHQhOp7Y1u164F3humoQDPu3np3DHzDqUQ");
     let data = req.body;
     let apiKey = '83e2602c775d467a916fb08017966d1c';
     let webhook = {};
@@ -13,36 +12,29 @@ module.exports = {
         webhook = await Webhook.create(data).fetch();
         let itemId = data.ItemId;
         let linkDetails = `${data.ServerUrl}web/index.html#!/details?id=${itemId}&serverId=${data.ServerId}`;
-        let image = `${data.ServerUrl}Items/${itemId}/Images/Backdrop?api_key=${apiKey}`;
-        let mensagem = `AdvanServer\n Novos ${
+        const image = `${data.ServerUrl}Items/${itemId}/Images/Primary?api_key=${apiKey}`;
+        const {data: getMediaInfo} = await axios.get(`https://advanflix.shop:8443/Users/026600e993bd42cd82676144e8c5d3d5/Items/${itemId}?api_key=${apiKey}`);
+        let format = getMediaInfo.MediaSources[0].Container;
+        let mensagem = `Novos ${
           data.ItemType === "Movie" ? "filmes" : "conteúdos"
         } adicionados:\n ${
           data.SeriesName !== undefined
             ? data.SeriesName + "\nTemporada " + data.SeasonNumber00 + " - "
             : ""
         } ${data.Name}
-        \nPara mais detalhes acesse o link abaixo:\n${linkDetails}`;
-
-        console.log(mensagem);
-        let response = await axios.get(image, { responseType: 'arraybuffer' });
-        console.log('axios: ', response)
-        let buffer = Buffer.from(response.data);
+        \nDetalhes:\n
+        \nFormato: ${format} \n
+        \nResolução: ${data.Video_0_Title} \n
+        \n${format !== "mp4" ? "<b>Recomendada Conversão Para MP4.</b>": ""} \n
+        \nPara mais detalhes acesse o link abaixo:\n`;
         // Envio de mensagem de texto para o Telegram
         const chatIds = [1655362615, 369036081];
-        //await sails.helpers.telegramBot(mensagem, chatIds);
-
-        // Obtenção do URL do pôster do Jellyfin usando o helper
-         // Obtenção do URL do pôster do Jellyfin usando o helper
-         
-           // Enviar a imagem para o Telegram usando telegraf
-           chatIds.forEach(chatId => bot.telegram.sendPhoto(chatId, { source: buffer }, { caption: mensagem }));
-         
+        await sails.helpers.telegramBot(mensagem, chatIds, image, linkDetails);
       } catch (error) {
         console.error('Erro ao processar o webhook:', error);
         return res.status(500).json({ error: 'Erro interno ao processar o webhook' });
       }
     }
-
     return res.status(200).json(webhook);
   },
 };
--- api/helpers/telegram-bot.js
+++ api/helpers/telegram-bot.js
@@ -1,6 +1,6 @@
 // api/helpers/telegramBot.js
-const { Telegraf } = require("telegraf");
-
+const { Telegraf, Markup } = require("telegraf");
+const axios = require('axios');
 module.exports = {
   friendlyName: "Telegram bot",
 
@@ -17,6 +17,16 @@ module.exports = {
       description: "Lista de IDs de chat para onde enviar a mensagem.",
       required: true,
     },
+    image: {
+      type: "string",
+      description: "Imagem do filme.",
+      required: true,
+    },
+    linkDetails: {
+      type: "string",
+      description: "link de detalhes do servidor.",
+      required: true,
+    }
   },
 
   exits: {
@@ -31,19 +41,47 @@ module.exports = {
   },
 
   fn: async function (inputs, exits) {
-    const { message, chatIds } = inputs;
-
+    const { message, chatIds, image, linkDetails } = inputs;
+    let apiKey = '83e2602c775d467a916fb08017966d1c';
+    const linkText = "Ver Detalhes"; // Texto do botão que abrirá o link
+    const linkUrl = linkDetails; // URL do link que será aberto pelo botão
     // Criar uma instância do bot com o token do seu bot
     const bot = new Telegraf("6932592891:AAHQhOp7Y1u164F3humoQDPu3np3DHzDqUQ");
+    let response = await axios.get(image, { responseType: 'arraybuffer' });
+    let buffer = Buffer.from(response.data);
+     // Formatar a mensagem
+     const  REPOSITORY_UPDATE = `https://advanflix.shop:8443/Library/Refresh?api_key=${apiKey}`;
+     
+     const formattedMessage = `${message}\n`;
+     // Montar o teclado inline com o botão do link
+     const keyboard = Markup.inlineKeyboard([
+      Markup.button.url(linkText, linkUrl),
+      Markup.button.callback("Atualizar Bibliotecas", "update_books"),
+     ]);
 
+     bot.action('update_books', async (ctx) => {
+      // Aqui você faria o pedido POST para sua API
+      try {
+        const response = await axios.post(REPOSITORY_UPDATE);
+        // Resposta da API
+        ctx.reply('Atualização iniciada com sucesso!');
+      } catch (error) {
+        // Erro ao fazer o pedido POST
+        ctx.reply('Houve um erro ao atualizar as bibliotecas.');
+      }
+    });
+    
     try {
       for (let id of chatIds) {
-        await bot.telegram.sendMessage(id, message);
+        await bot.telegram.sendPhoto(id, { source: buffer }, { caption: formattedMessage, parse_mode: 'HTML', reply_markup: keyboard.reply_markup });
       }
+      bot.launch();
       return exits.success("Mensagem enviada com sucesso");
     } catch (error) {
       console.error('Erro ao enviar mensagem para o Telegram:', error);
       return exits.error("Erro ao enviar a mensagem", error);
     }
+    
   },
+  
 };
